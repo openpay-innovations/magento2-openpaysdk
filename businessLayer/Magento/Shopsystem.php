f5bc91ea2c90b5e53be45d0ffc2664bb025f0536
elisabethSchacht <elisabeth.s@ideatarmac.com>
2021-10-03 04:10:45 +0200

    Sdk modification for virtual or gift product (#3)
    * Sdk modification for virtual or gift product
    
    * Removed duplicate code from ShopSystem.php
    
    * Changed cart delivery address object to delivery address
    
    Co-authored-by: saritakanaujia <saritakanaujia1@gmail.com>
    Co-authored-by: Sarita kanaujia <sarita.k@ideatarmac.com>
    Co-authored-by: babar-openpay <58763572+babar-openpay@users.noreply.github.com>

--- businessLayer/Magento/Shopsystem.php
+++ businessLayer/Magento/Shopsystem.php
@@ -19,7 +19,8 @@ class Shopsystem
      * @return object
      */
     public static function prepareShopCartObj($cart)
-    {  $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+    { 
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $currencysymbol = $objectManager->get('Magento\Store\Model\StoreManagerInterface');
         $currency = $currencysymbol->getStore()->getCurrentCurrencyCode();
         $customer = $objectManager->create('Magento\Customer\Model\Customer');
@@ -53,17 +54,69 @@ class Shopsystem
         $cart->integerTotal = ($integerTotal * 100);
         $cart->deliveryAddress = $cart->getShippingAddress()->getData();
         $cart->invoiceAddress = $cart->getBillingAddress()->getData();
+      
+        if ($deliveryAddress['street'] == null && 
+            $deliveryAddress['city'] == null &&
+            $deliveryAddress['region'] == null &&
+            $deliveryAddress['postcode'] == null
+        ) {
+            $cart->deliveryMethod = 'Email';
+        } else {
+            $cart->deliveryAddress = $deliveryAddress;
+            $deliveryAddress['city'] = empty($deliveryAddress['city']) ? '-' : $deliveryAddress['city'];
+            $deliveryAddress['postcode'] = empty($deliveryAddress['postcode']) ? '-' : $deliveryAddress['postcode'];
+            if (!empty($cart->deliveryAddress['region'])) {
+                $regionDelivery = $objectManager->create('Magento\Directory\Model\ResourceModel\Region\Collection')
+                    ->addRegionNameFilter($cart->deliveryAddress['region'])
+                    ->getFirstItem()
+                    ->toArray();
+                $cart->deliveryAddress['regioncode'] = (isset($regionDelivery['code'])) ? $regionDelivery['code'] : $cart->deliveryAddress['region'];
+            } else {
+                $cart->deliveryAddress['regioncode'] = '-';
+            }
+            if (!empty($cart->deliveryAddress['street'])) {
+                $linesDelivery = explode("\n", $cart->deliveryAddress['street']);
+                $cart->deliveryAddress['line1'] = $linesDelivery[0];
+                $linesDelivery2 = (isset($linesDelivery[1])) ? $linesDelivery[1] : '';
+                if (isset($linesDelivery[2])) {
+                    $linesDelivery2 .= ' ' . $linesDelivery[2];
+                }
+                $cart->deliveryAddress['line2'] = $linesDelivery2;
+            } else {
+                $cart->deliveryAddress['line1'] = '-';
+            }
+        }
+
+        // Get shipping address and billing address different or not
+        $billingAddress = array_diff($cart->invoiceAddress, $deliveryAddress);
+        if (array_key_exists('firstname', $billingAddress) && array_key_exists('lastname', $billingAddress)) {
+            $cart->deliveryAddress['firstname'] = $billingAddress['firstname'];
+            $cart->deliveryAddress['lastname'] = $billingAddress['lastname'];
+        }
+
+        /**
+         * This code is for onestepcheckout (START)
+         */
+        $request = $objectManager->get('Magento\Framework\App\RequestInterface');
+        $moduleManager = $objectManager->get('Magento\Framework\Module\Manager');
+        $isOnestepInstalled = $moduleManager->isEnabled('Onestepcheckout_Iosc');
+        if ($isOnestepInstalled) {
+            $dataManager = $objectManager->get('Onestepcheckout\Iosc\Model\DataManager');
+            $content = $request->getContent();
+            $payload = $dataManager->deserializeJsonPost($content);
+            $data = $dataManager->process($payload);
+            if ($data && !$data['error']) {
+                if (!$data['data']['billingAddress']) {
+                    $cart->invoiceAddress =$data['data']['shippingAddress'];
+                }
+            }
+        }
+        /** (END) */
+
         $cart->isoCurrency = $currency;
         $cart->cartId = $cart->getId();
         $cart->source = 'Magento';
-        
-        if (!empty($cart->deliveryAddress['region'])) {
-            $regionDelivery = $objectManager->create('Magento\Directory\Model\ResourceModel\Region\Collection')
-                ->addRegionNameFilter($cart->deliveryAddress['region'])
-                ->getFirstItem()
-                ->toArray();
-            $cart->deliveryAddress['regioncode'] = (isset($regionDelivery['code'])) ? $regionDelivery['code'] : $cart->deliveryAddress['region'];
-        }
+
         if (!empty($cart->invoiceAddress['region'])) {
             $regionInvoice = $objectManager->create('Magento\Directory\Model\ResourceModel\Region\Collection')
                 ->addRegionNameFilter($cart->invoiceAddress['region'])
@@ -79,14 +132,6 @@ class Shopsystem
             $linesInvoice2 .= ' ' . $linesInvoice[2];
         }
         $cart->invoiceAddress['line2'] = $linesInvoice2;
-        
-        $linesDelivery = explode("\n", $cart->deliveryAddress['street']);
-        $cart->deliveryAddress['line1'] = $linesDelivery[0];
-        $linesDelivery2 = (isset($linesDelivery[1])) ? $linesDelivery[1] : '';
-        if (isset($linesDelivery[2])) {
-            $linesDelivery2 .= ' ' . $linesDelivery[2];
-        }
-        $cart->deliveryAddress['line2'] = $linesDelivery2;
         
         return $cart;
     }
